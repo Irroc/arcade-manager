579e9e1b00f31862bd356133e6301a8a92db6e69
Corri Naglie <csnaglie@hotmail.com>
2024-01-20 13:05:08 -0500

    added schema and seeds

---
+++ models/location.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Location extends Model {}
+
+Location.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }, 
+    phone_number: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'locations',
+  }
+);
+
+module.exports = Location;
