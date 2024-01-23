81f54e85170360db57d827eb924dad52ebaf215d
Corri Naglie <csnaglie@hotmail.com>
2024-01-23 18:32:37 -0500

    added put request for a games location.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,16 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+  try {
+      const data = await User.findAll();
+
+      res.status(200).json(data);
+  } catch (err) {
+      res.status(500).json(err);
+  }
+});
 
 router.post('/', async (req, res) => {
   try {
