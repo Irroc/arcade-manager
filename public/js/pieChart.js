3a413928beb5ab5e7cada24d7aac20ee8762583f
SPCSanti1990 <81482804+SPCSanti1990@users.noreply.github.com>
2024-01-23 18:43:10 -0500

    added pieChart handlebars

--- public/js/pieChart.js
+++ public/js/pieChart.js
@@ -1,14 +1,15 @@
-import Chart from 'chart.js/auto';
+document.addEventListener('DOMContentLoaded', function () {
+    const data = {
+        labels: ['Blue', 'Red', 'Orange', 'Yellow', 'Green', 'Purple'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3]
+        }]
+    }
 
-const ctx = document.getElementById('pieChart').getContext('2d');
-new Chart(ctx, {
-  type: 'pie',
-  data: {
-    labels: ['Blue', 'Red', 'Orange', 'Yellow', 'Green', 'Purple'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
-  }
+    const ctx = document.getElementById('pieChart').getContext('2d');
+    const pieChart = new Chart(ctx, {
+        type: 'pie',
+        data: data
+    });
 });
