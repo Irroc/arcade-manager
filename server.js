84f1663ad3f1cda547253e1b03a151e602718d5d
SPCSanti1990 <81482804+SPCSanti1990@users.noreply.github.com>
2024-01-25 21:23:30 -0500

    excel.js created

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+const XLSX = require('xlsx');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -13,6 +14,8 @@ const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/arcade_manager_db', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const sess = {
   secret: 'Super secret secret',
@@ -45,6 +48,27 @@ app.get('/api/data', async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error '});
   }
+});
+
+app.get('/export', (req, res) => {
+  connection.query('SELECT * FROM your_table', (error, results, fields) => {
+    if (error) throw error;
+
+    // Process the data
+    const data = results.map((row) => [row.column1, row.column2, /* ... */]);
+
+    // Create a new workbook and add a worksheet
+    const workbook = XLSX.utils.book_new();
+    const worksheet = XLSX.utils.aoa_to_sheet([['Column1', 'Column2', /* ... */], ...data]);
+
+    // Add the worksheet to the workbook
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet 1');
+
+    // Save the workbook to a file
+    const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'buffer' });
+    res.attachment('data.xlsx');
+    res.send(excelBuffer);
+  });
 });
 
 app.use(routes);
